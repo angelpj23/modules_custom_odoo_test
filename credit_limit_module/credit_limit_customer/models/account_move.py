1933be3e5a3623d5e241907dd23421f4fe6dda1b
Angel Perez <angel_pdj@hotmail.com>
2022-12-20 17:03:53 -0400

    migrate_to_v12

--- credit_limit_module/credit_limit_customer/models/account_move.py
+++ credit_limit_module/credit_limit_customer/models/account_move.py
@@ -4,10 +4,14 @@ from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
-    _inherit = 'account.move'
 
-    applied_credit_limit = fields.Float(string='Limite de credito', compute='update_credit_limit',
-                                        help='Limite de credito establecido para este cliente')
+    _inherit = 'account.invoice'
+
+    applied_credit_limit = fields.Float(
+        string='Limite de credito',
+        compute='update_credit_limit',
+        help='Limite de credito establecido para este cliente',
+    )
 
     @api.onchange('partner_id')
     def update_credit_limit(self):
@@ -16,15 +20,35 @@ class SaleOrder(models.Model):
                 for s in self:
                     s.applied_credit_limit = self.partner_id.credit_limit
 
-                    if self.partner_id.block_SO == 0 and self.amount_total > self.applied_credit_limit or self.partner_id.block_SO == 0 and self.partner_id.total_debt >= self.applied_credit_limit:
-                        return {'value': {},
-                                'warning': {'title': 'Limite de credito',
-                                            'message': 'El cliente excede el limite de credito establecido: {}'.format(
-                                                s.applied_credit_limit)}}
+                    if (
+                        self.partner_id.block_SO == 0
+                        and self.amount_total > self.applied_credit_limit
+                        or self.partner_id.block_SO == 0
+                        and self.partner_id.total_debt >= self.applied_credit_limit
+                    ):
+                        return {
+                            'value': {},
+                            'warning': {
+                                'title': 'Limite de credito',
+                                'message': 'El cliente excede el limite de credito establecido: {}'.format(
+                                    s.applied_credit_limit
+                                ),
+                            },
+                        }
 
-                    elif self.partner_id.block_SO and self.amount_total > self.applied_credit_limit or self.partner_id.block_SO and self.partner_id.total_debt >= self.applied_credit_limit:
+                    elif (
+                        self.partner_id.block_SO
+                        and self.amount_total > self.applied_credit_limit
+                        or self.partner_id.block_SO
+                        and self.partner_id.total_debt >= self.applied_credit_limit
+                    ):
                         raise ValidationError(
-                            _('No puede facturar \nEl cliente excede el limite de credito establecido: {}'.format(s.applied_credit_limit)))
+                            _(
+                                'No puede facturar \nEl cliente excede el limite de credito establecido: {}'.format(
+                                    s.applied_credit_limit
+                                )
+                            )
+                        )
             else:
                 for s in self:
                     s.applied_credit_limit = 0
--- credit_limit_module/credit_limit_customer/models/res_partner.py
+++ credit_limit_module/credit_limit_customer/models/res_partner.py
@@ -6,15 +6,28 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     active_limit = fields.Boolean(string='Activar credito')
-    credit_limit = fields.Float(string='Limite de credito', help='Establezca el limite de credito para este cliente')
-    block_SO = fields.Boolean(string='Bloquear ventas', help='Bloquear las ventas cuando exceda el limite de credito')
-    total_debt = fields.Float(compute='update_debit')
+    credit_limit = fields.Float(
+        string='Limite de credito',
+        invisible="[('active_limit', '=', False)]",
+        help='Establezca el limite de credito para este cliente',
+    )
+    block_SO = fields.Boolean(
+        string='Bloquear ventas',
+        help='Bloquear las ventas cuando exceda el limite de credito',
+    )
+    total_debt = fields.Float(
+        compute='update_debit', invisible="[('active_limit', '=', False)]"
+    )
 
     @api.onchange('total_debt')
     def update_debit(self):
         for contact in self:
             customer = contact.id
-            invoices = self.env['account.move'].search(
-                [('partner_id.id', '=', customer), ('journal_id', '=', 1), ('state', '=', 'posted'),
-                 ('payment_state', 'in', ['not_paid', 'partial'])])
-            contact.total_debt = (sum(invoices.mapped('amount_total_signed')))
+            invoices = self.env['account.invoice'].search(
+                [
+                    ('partner_id.id', '=', customer),
+                    ('journal_id', '=', 1),
+                    ('state', 'in', ['posted', 'in_payment']),
+                ]
+            )
+            contact.total_debt = sum(invoices.mapped('amount_total_signed'))
